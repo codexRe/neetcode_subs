b03149b0706209b90be704940331b66bf10a404a
Adarsh Ranjan Gupta <adseemaad@gmail.com>
2026-09-05 23:00:16 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    string makehash(string &s){
+        vector <int> count(26);
+        for (char i: s)
+            ++count [i-'a'];
+        string hash = "";
+        for (int i: count){
+            hash.append(to_string(i));
+            hash.append(" ");
+        }
+        
+        return hash;
+    }
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map <string, int> groups;
+        for (string i: strs){
+            string hash = makehash(i);
+            if(groups.find(hash) == groups.end()){
+                groups[hash] = ans.size();
+                ans.push_back({});
+            }
+            ans[groups[hash]].push_back(i);
+        }
+        return ans;
+    }
+};
